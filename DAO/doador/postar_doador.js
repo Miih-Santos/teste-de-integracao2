3ae49d6258461dd85f384c1a8c28cb539a44f5e8
Martins-vini <viestudanteslk7@gmail.com>
2026-09-21 11:23:29 -0300

    Alteração em status e automatização de data de cadastro

--- DAO/doador/postar_doador.js
+++ DAO/doador/postar_doador.js
@@ -1,8 +1,10 @@
 async function postarCliente(conexao, {nome, email, senha, dataCadastro, ativo}){
-    let querySQL = `INSERT INTO cliente_doador_tbl (nome, email, senha, dataCadastro, ativo) VALUES (?,?,?,?,?)`;
-    let [doador] = await conexao.query(querySQL, [nome, email, senha, dataCadastro, ativo]);
+    let dataFinal = dataCadastro || new Date(); // Pega a data atual se não vier
+    let ativoFinal = ativo !== undefined ? ativo : true; // Garante true se não vier
+
+    let querySQL = `INSERT INTO cliente_doador_tbl (nome, email, senha, dataCadastro, ativo) VALUES (?, ?, ?, ?, ?)`;
+    let [doador] = await conexao.query(querySQL, [nome, email, senha, dataFinal, ativoFinal]);
     return doador.insertId;
 };
 
 export {postarCliente};
-
